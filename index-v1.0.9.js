dacc1dff0c8a08823e6606cb9903a326b18e9af5
K1ss_Lyak <138248934+nnetadon@users.noreply.github.com>
2023-09-24 21:53:47 +0300

    Update and rename index-v1.0.8.js to index-v1.0.9.js

--- index-v1.0.9.js
+++ index-v1.0.9.js
@@ -17455,7 +17455,7 @@ const Gx = new(Tx.default = Kx.default)({
                     }, n) : Q.jsx("div", {
                         className: "dashboard__top__item skeleton"
                     }, n)))	
-				*.
+				*/
 					
                 })
             }), Q.jsxs("div", {
