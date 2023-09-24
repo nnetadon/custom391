efa4e600a35f8d0576de5bc8ef7f5c132c9cb15a
K1ss_Lyak <138248934+nnetadon@users.noreply.github.com>
2023-09-24 21:15:01 +0300

    Update index-v1.0.6.js

--- index-v1.0.6.js
+++ index-v1.0.6.js
@@ -17441,7 +17441,7 @@ const Gx = new(Tx.default = Kx.default)({
                     className: "dashboard__top",
                     children: [Q.jsx(rS, {})]
 					// Отвечает за слайдер с привелегиями
-					/*
+					
 				Object.values(e).map(((e, n) => e ? Q.jsx("div", {
                         className: "dashboard__top__item",
                         style: {
@@ -17456,7 +17456,7 @@ const Gx = new(Tx.default = Kx.default)({
                         className: "dashboard__top__item skeleton"
                     }, n)))	
 				
-					*/
+					
                 })
             }), Q.jsxs("div", {
                 className: "dashboard__catalog",
